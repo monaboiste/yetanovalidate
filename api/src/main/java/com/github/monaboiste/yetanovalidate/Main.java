a3248a038b78eaca2ebb7b9f974323f4ee4127e6
Private <private@MAC-LQ7GMG97RG.local>
2024-01-17 00:02:16 +0100

    Init

---
+++ api/src/main/java/com/github/monaboiste/yetanovalidate/Main.java
@@ -0,0 +1,11 @@
+package com.github.monaboiste.yetanovalidate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    public void testMe() {
+        log.info("Testing... {}, {}, {}", 1, 2, 3);
+    }
+}
---
+++ api/src/test/java/com/github/monaboiste/yetanovalidate/MainTest.java
@@ -0,0 +1,11 @@
+package com.github.monaboiste.yetanovalidate;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test() {
+        new Main().testMe();
+    }
+}
